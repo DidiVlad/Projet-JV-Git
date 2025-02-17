af3e23c75e0a344d18f80e4fd911695a3b35652e
Gierczak Vladimir <vladimir.gierczak2@gmail.com>
2025-02-17 10:54:36 +0100

    door

--- Assets/Instances/DamageScript.cs
+++ Assets/Instances/DamageScript.cs
@@ -4,10 +4,8 @@ public class DamageScript : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision)
     {
-        print("triggered");
         if (collision.gameObject.CompareTag("Player"))
         {
-            print("touch player");
             Destroy(gameObject);
             collision.gameObject.GetComponent<PlayerHealthMain>().HP -= 1; 
         }
